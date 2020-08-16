0f87da4c70a0c1a5105b3fea15035a480c0b79f2
dadaychua <juv.chua@gmail.com>
2020-08-16 23:51:33 +0800

    Adds template for login and registration

--- bulletin_board/urls.py
+++ bulletin_board/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from board import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login, name='Login'),
+    path('registration/', views.registration, name='registration')
 ]
